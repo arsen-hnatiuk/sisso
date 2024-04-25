89db41f3e473dbd44ed8ad9553480ae703cdb6a9
arsen-hnatiuk <arsen.hnatiuk@hu-berlin.de>
2024-04-25 11:13:24 +0200

    feat: debugged implementation

--- src/sisso.py
+++ src/sisso.py
@@ -1,6 +1,7 @@
 import numpy as np
 import logging
 from itertools import combinations
+import random
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -15,10 +16,10 @@ class SISSO:
         self.data_size = len(y)
 
     def fit(self, tol: float = 0.01, max_iterations: float = 5) -> np.ndarray:
-        sis_subspace_size = (
-            np.exp(self.data_size / (3.125 * max_iterations)) / max_iterations
+        sis_subspace_size = max(
+            int(np.exp(self.data_size / (3.125 * max_iterations)) / max_iterations), 1
         )
-        active_set = np.array([])
+        active_set = np.array([], dtype=int)
         iterate = np.zeros(self.data_size)
         residual = iterate - self.y
         error = np.linalg.norm(residual)
@@ -40,10 +41,18 @@ class SISSO:
             combinatorial_counter = 0
             for combination in combinatorial_combinations:
                 combinatorial_counter += 1
-                coefficients = self.K[:, np.array(combination)]
-                least_squares, res, rank, s = np.linalg.lstsq(coefficients, self.y)
-                if np.sqrt(res) < min_error:
-                    min_error = np.sqrt(res)
+                submatrix = self.K[:, np.array(combination)]
+                least_squares, res, rank, s = np.linalg.lstsq(
+                    submatrix, self.y, rcond=None
+                )
+                if len(res):
+                    local_error = np.sqrt(res[0])
+                else:
+                    local_error = np.linalg.norm(
+                        np.matmul(submatrix, least_squares) - self.y
+                    )
+                if local_error < min_error:
+                    min_error = local_error
                     optimal_combination = combination
                     optimal_coefficients = least_squares
             iterate = np.matmul(
@@ -59,3 +68,17 @@ class SISSO:
             logging.info(f"Optimal combination: {optimal_combination}")
             logging.info(f"Optimal coefficients: {optimal_coefficients}")
             n += 1
+
+
+# if __name__ == "__main__":
+#     data_size = 100
+#     m = 1000000
+#     K = np.array(
+#         [
+#             np.random.normal(random.uniform(-3, 3), random.uniform(0.5, 3), [m])
+#             for _ in range(data_size)
+#         ]
+#     )
+#     y = np.random.normal(random.uniform(-3, 3), random.uniform(0.5, 3), [data_size])
+#     sisso = SISSO(K, y)
+#     sisso.fit()
